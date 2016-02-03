c967c42a536af7bd004c21bc44dbf340288d58d3
davis <davissuperman@gmail.com>
2016-02-03 11:32:38 +0800

    invokeMethod

--- phpUnitTutorial/Test/UserTest.php
+++ phpUnitTutorial/Test/UserTest.php
@@ -30,6 +30,33 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $currentUser = $user->getUser();
 
         $this->assertEquals($expectedPasswordResult, $currentUser['password']);
-
+        return $user;
     }
+    /**
+     *  @depends testGetUserReturnsUserWithExpectedValues
+     */
+    public function testMd5($user){
+
+     echo  "\n".  $this->invokeMethod($user, 'cryptPassword', array('passwordToCrypt'));
+    }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+
 }
