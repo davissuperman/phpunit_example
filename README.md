ad4274f83fe71fdf4731ee163147ff90966d6051
davis <davissuperman@gmail.com>
2016-02-03 11:00:23 +0800

    add depend

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# phpunit_example
+1. 如果没有高亮显示 则运行此命令
+ phpunit  URLTest.php  --colors=always
--- phpUnitTutorial/Test/URLTest.php
+++ phpUnitTutorial/Test/URLTest.php
@@ -15,4 +15,27 @@ class URLTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expectedResult, $result);
     }
+
+    /**
+     * @dataProvider providerTestFoo
+     */
+    public function testFoo($variableOne, $variableTwo)
+    {
+        //
+        echo $variableOne." \n";
+        echo $variableTwo." \n";
+    }
+
+    public function providerTestFoo()
+    {
+        return array(array(1,2));
+//        return array(
+//            array('test 1, variable one', 'test 1, variable two'),
+//            array('test 2, variable one', 'test 2, variable two'),
+//            array('test 3, variable one', 'test 3, variable two'),
+//            array('test 4, variable one', 'test 4, variable two'),
+//            array('test 5, variable one', 'test 5, variable two'),
+//        );
+    }
+
 }
