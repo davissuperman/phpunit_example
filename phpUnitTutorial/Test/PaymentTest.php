fa8e5579ef6a690e7cde3234f5da28acdbf1552a
davis <davissuperman@gmail.com>
2016-02-04 10:04:04 +0800

    add

--- phpUnitTutorial/Test/PaymentTest.php
+++ phpUnitTutorial/Test/PaymentTest.php
@@ -1,6 +1,17 @@
 <?php
+namespace phpUnitTutorial\Test;
+use phpUnitTutorial\Payment;
 class PaymentTest extends \PHPUnit_Framework_TestCase{
     public function testProcessPayment(){
-        
+        $paymentDetails = array(
+            'amount'   => 123.99,
+            'card_num' => '4111-1111-1111-1111',
+            'exp_date' => '03/2013',
+        );
+
+        $payment = new Payment();
+        $result = $payment->processPayment($paymentDetails);
+
+        $this->assertTrue($result);
     }
 }
